82da4b2a59b125da2266ec29a943e6c2ef1ce5e7
Vadim Dalecky <streamich@gmail.com>
2017-12-20 22:09:08 +0000

    chore: update Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,42 @@
-var webpack = require('webpack');
-var nodeExternals = require('webpack-node-externals');
-var WrapperPlugin = require('wrapper-webpack-plugin');
-
-
-var globals_plugin = new webpack.DefinePlugin({
-    // DEBUG mode
-    __DEBUG__: JSON.stringify(JSON.parse(process.env.BUILD_DEBUG || true)),
-
-    // Whether to trace syscalls.
-    __STRACE__: JSON.stringify(JSON.parse(process.env.BUILD_STRACE || true)),
-
-    // `console.log()` will not print to terminal if `true`.
-    __STRACE_BLOCK_STDOUT__: JSON.stringify(JSON.parse(process.env.BUILD_STRACE_BLOCK_STDOUT || false)),
-
-    // Whether to create a thread pool to make async syscall function.
-    __BUILD_ASYNC_SYSCALL__: JSON.stringify(JSON.parse(process.env.BUILD_BUILD_ASYNCSYSCALL || true)),
-});
-
+const path = require('path');
+const webpack = require('webpack');
+const WrapperPlugin = require('wrapper-webpack-plugin');
 
 module.exports = {
     entry: {
-        app: './src/index'
+        bamboo: './src/index'
     },
     output: {
-        path: './dist',
-        filename: 'full.js'
+        path: path.join(__dirname, 'dist'),
+        filename: '[name].js'
     },
     resolve: {
-        extensions: ['.js']
+        extensions: ['.ts', '.js']
     },
-    target: 'node',                 // in order to ignore built-in modules like path, fs, etc.
-    // externals: [nodeExternals()]    // in order to ignore all modules in node_modules folder
-
+    module: {
+        loaders: [
+            {test: /\.ts$/, loader: 'ts-loader'}
+        ],
+    },
+    target: 'node', // in order to ignore built-in modules like path, fs, etc.
     plugins: [
         new WrapperPlugin({
             header: 'global = this;\n',
             footer: ''
         }),
-        globals_plugin
+        new webpack.DefinePlugin({
+            // DEBUG mode
+            __DEBUG__: JSON.stringify(JSON.parse(process.env.BAMBOO_DEBUG || false)),
+        
+            // Whether to trace syscalls.
+            __STRACE__: JSON.stringify(JSON.parse(process.env.BAMBOO_STRACE || false)),
+        
+            // `console.log()` will not print to terminal if `true`.
+            __STRACE_BLOCK_STDOUT__: JSON.stringify(JSON.parse(process.env.BAMBOO_NO_STDOUT || false)),
+        
+            // Whether to create a thread pool to make async syscall function `process.asyscall`,
+            // if it is ont provided by JavaScript runtime.
+            __BUILD_ASYNC_SYSCALL__: JSON.stringify(JSON.parse(process.env.BAMBOO_ASYSCALL || true)),
+        }),
     ]
 };
